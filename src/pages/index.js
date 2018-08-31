2a1354becca3585bb52ede75cd613aeb3d93a0de
Severiano Badajoz <sevebadajoz@gmail.com>
2018-08-31 16:46:36 -0700

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Severiano Badajoz</h1>
+    <p>Severiano "Seve" Badajoz is a prospective Computer Science student.
+     Developing in the heart of San Francisco.</p>
+    <p>Originally from Orange County, CA,  Seve transfered from Orange Coast College to Make School to continue his higher education career.
+        Here he persues to widen his developing breadth to include full stack web development,
+        with an emphasis on front end.
+    </p>
+    <p> Seve is incredibly excited to enter freeCodeCamp's JAMstack hackathon as his first ever hackathon!</p>
+
+  </div>
+)
+
+export default IndexPage
